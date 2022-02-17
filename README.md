61887a812745be82868f3c2f7354b5099b60101b
Andrew Symes <andrew.symes1@ucalgary.ca>
2022-02-16 22:18:55 -0700

    cleaned up folder

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Symes-651-Lab2
 Lab 2 by Andrew Symes
+30033743
 
-Web mapping application 
+Web mapping application
+
+Files:
+application.py - Python file that handles the flask app and does data querying and cleaning
+
+templates/Mappingpage.html - Mapping page where the map displaying data requested by the user is presented
--- application.py
+++ application.py
@@ -1,23 +1,29 @@
+#ENGO 651 Lab 2
+#Andrew Symes
+#UCID 30033743
 
+#imports, More than is needed but ok
 import os
 import re
 import requests
 import json
-
-
 from flask import Flask, session
 from flask import render_template, request
 from flask import request
 from flask_session import Session
 import pandas as pd
+#install sodapy
 from sodapy import Socrata
 
 app = Flask(__name__)
 
+#Homepage, and only page
 @app.route("/", methods = ["GET", "POST"])
 def index():
+    #Code modified from Socrata tutorial available at https://dev.socrata.com/blog/2014/11/04/data-visualization-with-python.html
     # Unauthenticated client only works with public data sets. Note 'None'
     # in place of application token, and no username or password:
+    #Data information provided through https://dev.socrata.com/foundry/data.calgary.ca/c2es-76ed
     client = Socrata("data.calgary.ca", None)
 
     # Example authenticated client (needed for non-public datasets):
@@ -49,6 +55,7 @@ def index():
     df['longitude'] = df['longitude'].astype(float)
 
 #Bump same place values a little bit to make it nicer.
+#This is my simple approach to requirement 4
     for i in range(0,len(df['latitude'])):
         for j in range(0,len(df['longitude'])):
             if df['latitude'][i] == df['latitude'][j]:
@@ -94,7 +101,8 @@ def index():
 
     return render_template("Mappingpage.html",Gengeojson=Gengeojson)
 
-
+#the above code transforming the query to geoJSON was adapted from the code providded at
+#https://notebook.community/captainsafia/nteract/applications/desktop/example-notebooks/pandas-to-geojson
 
 
 
